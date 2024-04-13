18dd7bf69e080eb07bee0166ea679f9a197c93bf
Dimitar Vasilev <captaingetrekt@gmail.com>
2024-04-13 16:24:27 +0300

    changed UI tests url to be the of the deployed app

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("Check cookbook page", async ({ page }) => {
-    await page.goto("http://localhost:8080/cookbook");
+    await page.goto("https://dimitarv23-cookbook.onrender.com/cookbook");
     const list = await page.$("ul");
     expect(list).toBeTruthy();
 });
